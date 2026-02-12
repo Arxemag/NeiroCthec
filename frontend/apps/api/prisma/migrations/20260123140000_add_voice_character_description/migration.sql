0f59c8103aea21d029b8ee863d021b734bf31f2f
Arxemag <igor9105@gmail.com>
2026-02-12 21:20:08 +0300

    Сборка проекта

--- frontend/apps/api/prisma/migrations/20260123140000_add_voice_character_description/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Voice" ADD COLUMN "characterDescription" TEXT;
--- frontend/apps/api/src/modules/admin/dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsString, MinLength } from 'class-validator';
-
-export class AdminChangePasswordDto {
-  @IsString()
-  @MinLength(8, { message: 'Пароль не менее 8 символов' })
-  newPassword!: string;
-}
